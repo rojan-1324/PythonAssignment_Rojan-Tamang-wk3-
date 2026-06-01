8ca8907dba50f398727c3dc59a76fabe9c16f33c
rojan-1324 <tmgrojan399@gmail.com>
2026-06-01 14:56:31 +0545

    Add files via upload

---
+++ PRG200_Python Assignment/Assignment wk4/waterlevelwarning.py
@@ -0,0 +1,21 @@
+sensors=[
+("Chatara",2.8),
+("Tribeni Ghat",5.4),
+("Koshi Barrage",4.1),
+("Sunsari Bridge",1.9),
+("Saptakoshi Camp",6.0)
+]
+
+def check_water_level(location,level_metres):
+
+ if level_metres<3:
+  return "Safe"
+
+ elif level_metres<=5:
+  return "Warning - Alert nearby villages"
+
+ else:
+  return "DANGER - Evacuate immediately!"
+
+for place,level in sensors:
+ print(place,"("+str(level)+" m):",check_water_level(place,level))
